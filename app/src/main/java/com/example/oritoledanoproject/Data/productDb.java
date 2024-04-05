0724d989c0efe868431ca405f663bf0907e63340
oritoledano <oritoledano123@gmail.com>
2024-04-05 11:45:00 +0300

    כפתור חזרה

--- app/src/main/java/com/example/oritoledanoproject/Data/productDb.java
+++ app/src/main/java/com/example/oritoledanoproject/Data/productDb.java
@@ -2,8 +2,16 @@ package com.example.oritoledanoproject.Data;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class productDb {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    Map<Szbdtring, Object> user = new HashMap<>();
+    user.put("first", );
+    user.put("last", );
+    user.put("born", );
 
 
 }
